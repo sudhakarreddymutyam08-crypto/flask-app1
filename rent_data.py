0612c4fe1c4ab4b0b609cd8ed7750519ff5c951e
sudhakarreddymutyam08-crypto <sudhakarreddymutyam08@gmail.com>
2026-03-31 23:50:47 +0100

    Added API integration for rental data fetching
    Reduced the number of printed dataset results from 5 to 3 and removed CSV extraction and saving logic.

--- rent_data.py
+++ rent_data.py
@@ -1,37 +1,11 @@
 import requests
-import pandas as pd
 
-# API call
 url = "https://data.gov.ie/api/3/action/package_search?q=rent"
 
 response = requests.get(url)
 data = response.json()
 
-# Print datasets
-for result in data['result']['results'][:5]:
+for result in data['result']['results'][:3]:
     print("Title:", result['title'])
     print("Organization:", result['organization']['title'])
     print("-"*40)
-
-# Extract CSV
-csv_url = None
-
-for result in data['result']['results']:
-    for resource in result['resources']:
-        if resource['format'].lower() == 'csv':
-            csv_url = resource['url']
-            break
-    if csv_url:
-        break
-
-print("Using CSV:", csv_url)
-
-# Load data
-df = pd.read_csv(csv_url)
-
-print(df.head())
-
-# Save CSV
-df.to_csv("rent_data.csv", index=False)
-
-print("CSV saved successfully!")
