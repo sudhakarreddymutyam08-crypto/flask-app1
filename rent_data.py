4f4670c769ca9fde4eb57185f5390b66bfead2f5
sudhakarreddymutyam08-crypto <sudhakarreddymutyam08@gmail.com>
2026-04-01 00:50:31 +0100

    Add functionality to read CSV data from Google Sheets

--- rent_data.py
+++ rent_data.py
@@ -9,3 +9,11 @@ for result in data['result']['results'][:3]:
     print("Title:", result['title'])
     print("Organization:", result['organization']['title'])
     print("-"*40)
+    
+import pandas as pd
+
+url = "https://docs.google.com/spreadsheets/d/1IUVKA-a4fblWawCmlhJyTrQZ3e1R1Nvm3D9bXksk0GM/gviz/tq?tqx=out:csv&gid=0"
+
+df = pd.read_csv(url)
+
+print(df.head())
