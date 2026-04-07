22dbeab99ccb403f24e55200f5a9a8517716b281
sudhakarreddymutyam08-crypto <sudhakarreddymutyam08@gmail.com>
2026-04-07 22:20:28 +0100

    Implement data cleaning in clean_data function
    Added data cleaning functionality to read, clean, and save rent data.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,14 @@
 import pandas as pd
 def clean_data():
-  pass
+  df=pd.read_csv("rent_data.csv")
+  print("Before cleaning:"df.shape)
+  df=df.dropna()
+  df=df.drop_duplicates()
+  print("After Cleaning:"df.shape)
+  df.to_csv("cleaned rent data.csv",index=False)
+  print("clean file saved")
+
+if __name__=="__main__":
+  clean_data() 
+  
+  
