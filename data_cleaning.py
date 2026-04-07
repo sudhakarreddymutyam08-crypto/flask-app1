42176039bd0c0200481347500f1f8c33d4bfa59c
sudhakarreddymutyam08-crypto <sudhakarreddymutyam08@gmail.com>
2026-04-07 22:34:07 +0100

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def clean_data():
   df=pd.read_csv("rent_data.csv")
-  print("Before cleaning:"df.shape)
+  print("Before cleaning:",df.shape)
   df=df.dropna()
   df=df.drop_duplicates()
   print("After Cleaning:",df.shape)
