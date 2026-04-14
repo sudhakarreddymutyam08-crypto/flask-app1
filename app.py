5ffc620d185e54018d04e8ae6bcae765ed87330b
sudhakarreddymutyam08-crypto <sudhakarreddymutyam08@gmail.com>
2026-04-14 01:17:59 +0100

    Initialize Flask app with SQLite data retrieval

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import sqlite3
+import pandas as pd
+
+app = Flask(__name__)
+
+# function to get small data (FAST)
+def get_data():
+    conn = sqlite3.connect("rent_data.db")
+
+    # only load small data → FAST
+    df = pd.read_sql("SELECT * FROM rent_table LIMIT 200", conn)
+
+    conn.close()
+    return df
+
+@app.route("/")
+def home():
+    df = get_data()
+
+    data = df.to_dict(orient="records")
+
+    # simple stats
+    avg_rent = round(df['VALUE'].mean(), 2)
+    total = len(df)
+
+    return render_template(
+        "index.html",
+        data=data,
+        avg_rent=avg_rent,
+        total=total
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
